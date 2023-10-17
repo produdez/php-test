1b6589f0e075c7164e5fba7750f8d24ac0a56b9a
produdez <bigtimegangstar@gmail.com>
2023-10-17 09:04:39 +0700

    Experiment setup

---
+++ components/README.md
@@ -0,0 +1,12 @@
+# Components
+
+Just static components
+
+Include and create component with
+
+```[php]
+<?php
+ include '../../components/header.php';
+ echo createHeader();
+?>
+```
---
+++ components/header.php
@@ -0,0 +1,13 @@
+<?php 
+  function createHeader() {
+    return '
+      <header>
+            <h1> 
+                <span class="emoji">🚀</span> 
+                Reusable Static Header
+                <span class="emoji">🚀</span>
+            </h1>
+        </header>
+    ';
+  }
+?>
