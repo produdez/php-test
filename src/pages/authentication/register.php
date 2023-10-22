8e610ad347dc5f8aa80d17d85f14a7ef0771f57d
produdez <bigtimegangstar@gmail.com>
2023-10-22 16:09:58 +0700

    [feat] register pages

---
+++ src/pages/authentication/register.php
@@ -0,0 +1,45 @@
+<?php
+	// Component variables
+	$c_meta_title = "Register Page";
+
+	// Import reusable component
+	require_once '../../components/Button.php';
+	require_once '../../components/MessageBanner.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- head tag included with above c_meta_title variable -->
+<?php include '../../components/meta.php'?>
+
+<body>
+	<!-- Raw included header -->
+	<?php include '../../components/header.php'?>
+	<?php MessageBanner() ?>
+
+	<div class="min-h-screen center-page">
+		<div class="w-5/6 center-container self-center p-10">
+			<h2> Register </h2>
+			<!-- Reusable functional component -->
+			<form action="register_post.php" method="post">
+				<p>Username</p>
+				<input type="text" name="username"  required>
+				<p>Password</p>
+				<input type="password" name="password" required>
+				<p> Full Name </p>
+				<input type="text" name="fullName">
+				
+				<br><br>
+				<button> Submit </button>
+			</form>
+			<div>
+				<p> Have an account already? <a href="./login.php"> Login </a></p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Raw included footer -->
+	<?php include '../../components/footer.php'?>
+</body>
+</html>
+
---
+++ src/pages/authentication/register_post.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/src/utils/urlUtils.php";
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/bootstrap.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/src/models/User.php";
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$fullName = $_POST['fullName'];
+
+
+try {
+	if(!$username || !$password) throw new Error('Credentials cannot be empty');
+
+	$userRepository = $entityManager->getRepository('User');
+	$user = $userRepository->findOneBy(array('username' => $username));
+	if($user != null) {
+		throw new Error("User Already Exists");
+	}
+	
+	// TODO: add more server-side validations + encryption
+	$user = new User();
+	$user->setUsername($username);
+	$user->setPassword($password);
+	$user->setFullName($fullName);
+	
+	$entityManager->persist($user);
+	$entityManager->flush();
+
+	URLUtils::redirectWithMessage('./login.php', 'info', 'Account registered, please login.');
+} catch (\Throwable $error) {
+	URLUtils::redirectWithMessage('./register.php', 'error', $error->getMessage());
+}
