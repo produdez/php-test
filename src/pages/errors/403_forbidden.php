75dbdb870c90b20ac3ca0ae776374bff43445d60
produdez <bigtimegangstar@gmail.com>
2023-10-22 16:07:21 +0700

    [feat] error page

---
+++ src/pages/errors/403_forbidden.php
@@ -0,0 +1,27 @@
+<?php
+	// Component variables
+	$c_meta_title = "403_forbidden";
+
+	// Import reusable component
+	include '../../components/Button.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- head tag included with above c_meta_title variable -->
+<?php include '../../components/meta.php'?>
+
+<body>
+	<!-- Raw included header -->
+	<?php include '../../components/header.php'?>
+
+	<div class="w-screen bg-red-100">
+		<div class="flex flex-col items-center text-center w-10/12 h-screen  mx-auto">
+			<h1 class=" text-red-600 text-3xl font-bold p-5">Forbidden !!!</h1>
+		</div>
+	</div>
+
+	<!-- Raw included footer -->
+	<?php include '../../components/footer.php'?>
+</body>
+</html>
+
